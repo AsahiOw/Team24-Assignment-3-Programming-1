bba157d4e5e0247a9692b69d6638c1c7c285d876
Nguyễn Hà Tuấn Nguyên <118656767+AsahiOw@users.noreply.github.com>
2023-09-19 17:18:27 +0700

    update on manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import Class.Port;
+import Class.*;
+
 
 public class Manager extends User {
     private Port managedPort;
@@ -14,22 +15,52 @@ public class Manager extends User {
     //    public Port managedPort;
     @Override
     public void showMenuOptions() {
+        int sub_option = 0;
         System.out.println("Welcome Manager of Port " + managedPort.getName() + "! Select an option:");
-        System.out.println("1. List vehicles in this port");
-        System.out.println("2. List containers in this port");
-        System.out.println("3. Add new vehicle");
-        System.out.println("4. Add new container");
-        System.out.println("5. Remove vehicle");
-        System.out.println("6. Remove container");
-        System.out.println("7. Vehicle load container");
-        System.out.println("8. Vehicle unload container");
-        System.out.println("9. Exit");
+        System.out.println("1. Listing Container in port");
+        System.out.println("2. Add Container in port");
+        System.out.println("3. Remove Container in port");
+        System.out.println("4. Statistic Operation");
+        System.out.println("5. Exit");
 
         // Get input choice and execute relevant method
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("\nSelect Option: ");
         int option = scanner.nextInt();
-    }
+        switch (option) {
+            case 1:
+                managedPort.getOnPortContainers();
+                continueToOption();
+                break;
 
+            case 2:
+                System.out.print("Enter container weight: ");
+                double Weight = scanner.nextDouble();
+
+                System.out.print("Enter container type: ");
+                break;
+            case 3:
+                managedPort.getOnPortContainers();
+
+                System.out.print("Enter id of the container want to remove:");
+                String idToRemove = scanner.nextLine();
+
+                Container.removeContainer(idToRemove);
+                break;
+        }
+    }
+    public void continueToOption() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nContinue? (Y/N) ");
+        String option = scanner.nextLine();
+
+        if(option.equalsIgnoreCase("Y")) {
+            showMenuOptions();
+        } else if(option.equalsIgnoreCase("N")) {
+            System.out.println("Session end.");
+            return;
+        }
+    }
 }
